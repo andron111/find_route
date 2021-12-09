8a5f381ff0aa344fd64c8438ee7360f1db15c96a
YOUR_NAME <YOUR_EMAIL>
2021-12-09 23:27:47 +0300

    add

--- src/travel/settings.py
+++ src/travel/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'travel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR.joinpath('templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ src/travel/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    name = 'Andrei'
+    return render(request, 'home.html', {'name': name})
+
+
+def about(request):
+    name = 'Nadia'
+    return render(request, 'about.html', {'name': name})

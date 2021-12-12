43867b1c0ad660e37997f3aa0e14e169662fb6a9
YOUR_NAME <YOUR_EMAIL>
2021-12-12 23:07:14 +0300

    add

--- src/cities/urls.py
+++ src/cities/urls.py
@@ -4,5 +4,7 @@ from cities.views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('<int:pk>/', home, name='home'),
+
 
 ]
--- src/cities/views.py
+++ src/cities/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from cities.models import City
 
@@ -7,7 +7,14 @@ __all__ = (
 )
 
 
-def home(request):
+def home(request, pk=None):
+    if pk:
+        # city = City.objects.filter(id=pk).first()
+        # city = City.objects.get(id=pk)
+        city = get_object_or_404(City, id=pk)
+
+        context = {'object': city}
+        return render(request, 'cities/detail.html', context)
     qs = City.objects.all()
     context = {'objects_list': qs}
     return render(request, 'cities/home.html', context)

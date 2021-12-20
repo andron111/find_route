99769067f9d8f8f20f9e7449981354cb2d5b917d
Web_1 <kli4ev2015@yandex.ru>
2021-12-20 16:09:49 +0300

    add

--- src/routes/utils.py
+++ src/routes/utils.py
@@ -1,4 +1,48 @@
+from turtledemo.forest import start
+
+from trains.models import Train
+
+
+def dfs_paths(graph, starts, goal):
+    stack = [(start, [start])]
+    while stack:
+        (vertex, path) = stack.pop()
+        if vertex in graph.keys():
+            for next_ in graph[vertex] - set(path):
+                if next_ == goal:
+                    yield path + [next_]
+                else:
+                    stack.append((next_, path + [next_]))
+
+
+def get_graph(qs):
+    graph = {}
+    for q in qs:
+        graph.setdefault(q.from_city_id, set())
+        graph[q.from_city_id].add(q.to_city_id)
+    return graph
 
 
 def get_routs(request, form) -> dict:
-    pass
+    context = {'form': form}
+    qs = Train.objects.all()
+    graph = get_graph(qs)
+    data = form.cleaned_data
+    from_city = data['from_city']
+    to_city = data['to_city']
+    cities = data['cities']
+    travelling_time = data['travelling_time']
+    all_ways = list(dfs_paths(graph, from_city.id, to_city.id))
+    if not len(all_ways):
+        raise ValueError('Маршрута не существует')
+    if cities:
+        _cities = [city.id for city in cities]
+        right_ways = []
+        for route in all_ways:
+            if all(city in route for city in cities):
+                right_ways.append(route)
+        if not right_ways:
+            raise ValueError("Маршрут через эти города невозможен")
+    return context
+
+
